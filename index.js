45fbcce64289ed0eadd4663f5a29927cdb7c5180
Lucas Gasenzer <lucasgasenzer@mac.com>
2016-03-21 21:34:35 +0100

    Initial commit
    Initial version containing Power Consumption and Total Consumption (per day)

---
+++ index.js
@@ -0,0 +1,125 @@
+var request = require("request");
+var inherits = require('util').inherits;
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-efergy", "Efergy", Efergy);
+
+
+  function Efergy(log, config) {
+    // configuration
+    this.token = config['token'];
+    this.name = config['name'];
+
+    this.log = log;
+
+    this.kWh_url = "http://www.energyhive.com/mobile_proxy/getEnergy?token=" + this.token + "&period=day&offset=-60";
+    this.W_url = "http://www.energyhive.com/mobile_proxy/getCurrentValuesSummary?token=" + this.token;
+
+  }
+
+  // Custom Characteristics and service...
+  Efergy.PowerConsumption = function() {
+    Characteristic.call(this, 'Watts', 'E863F10D-079E-48FF-8F27-9C2605A29F52');
+    this.setProps({
+      format: Characteristic.Formats.FLOAT,
+      maxValue: 999999999,
+      minValue: 1,
+      minStep: 0.001,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(Efergy.PowerConsumption, Characteristic);
+
+  Efergy.TotalConsumption = function() {
+    Characteristic.call(this, 'kWh', 'E863F10C-079E-48FF-8F27-9C2605A29F52');
+    this.setProps({
+      format: Characteristic.Formats.FLOAT,
+      maxValue: 999999999,
+      minValue: 1,
+      minStep: 0.001,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(Efergy.TotalConsumption, Characteristic);
+
+  Efergy.PowerService = function(displayName, subtype) {
+    Service.call(this, displayName, '00000001-0000-1000-8000-135D67EC4377', subtype);
+    this.addCharacteristic(Efergy.PowerConsumption);
+    this.addCharacteristic(Efergy.TotalConsumption);
+  };
+  inherits(Efergy.PowerService, Service);
+
+  Efergy.prototype = {
+
+    httpRequest: function(url, method, callback) {
+      request({
+        url: url,
+        method: method
+      },
+      function (error, response, body) {
+        callback(error, response, body)
+      })
+    },
+
+    getConsumption: function(callback) {
+      this.httpRequest(this.W_url, 'get', function(error, response, body) {
+        if (error) {
+          this.log('HTTP function failed: %s', error);
+          callback(error);
+        }
+        else {
+          // stupidly complex because of the way the
+          // json is formated by the server...
+          var json = JSON.parse(body);
+          var obj = json[0].data[0];
+          var key = Object.keys(obj)[0];
+          var data = obj[key];
+          this.log('Read Total Conumption:', data, 'W');
+          callback(null, data);
+        }
+      }.bind(this))
+    },
+
+    getTotalConsumption: function(callback) {
+      this.httpRequest(this.kWh_url, 'get', function(error, response, body) {
+        if (error) {
+          this.log('HTTP function failed: %s', error);
+          callback(error);
+        }
+        else {
+          var json = JSON.parse(body);
+          var kWh = parseFloat(json['sum']);
+          this.log('Read Total Consumption:', kWh, 'kWh today');
+          callback(null, kWh);
+        }
+      }.bind(this))
+    },
+
+    getServices: function() {
+		    var that = this;
+
+		      var informationService = new Service.AccessoryInformation();
+		        informationService
+			         .setCharacteristic(Characteristic.Name, this.name)
+	    		     .setCharacteristic(Characteristic.Manufacturer, "Efergy")
+	    		     .setCharacteristic(Characteristic.Model, "Unknown")
+	    	       .setCharacteristic(Characteristic.SerialNumber, "1234567890");
+
+			var myPowerService = new Efergy.PowerService("Power Functions");
+			myPowerService
+				.getCharacteristic(Efergy.PowerConsumption)
+				.on('get', this.getConsumption.bind(this));
+      myPowerService
+        .getCharacteristic(Efergy.TotalConsumption)
+        .on('get', this.getTotalConsumption.bind(this));
+
+			return [informationService, myPowerService];
+		}
+	}
+}
